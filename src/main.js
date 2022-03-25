04888431b396104ac2d60e43745f73446c0c66d3
QxsDong <qxs5138815@163.com>
2022-03-25 16:00:27 +0800

    zzzz

--- src/main.js
+++ src/main.js
@@ -28,7 +28,7 @@ Vue.prototype.$axios = axios;
 Vue.config.productionTip = false
 
 //配置默认地址
-localStorage.setItem('sysOrderNum', 'API150724786641712332');
+localStorage.setItem('sysOrderNum', 'API150725968421257625');
 localStorage.setItem('baseUrl', 'http://47.243.170.64:9080');
 // localStorage.setItem('baseUrl', 'https://service.alchemypay.cc');
 // localStorage.setItem('baseUrl', 'https://paytest.alchemypay.cc');
